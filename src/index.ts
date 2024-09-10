9ddf57dad68537d25ec851e68d1850381c9c0e3c
Adebayo Temi <temiq3@gmail.com>
2024-09-10 10:31:55 +0100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database"));
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health ok!" });
+});
+
+app.use("/api/my/user", myUserRoute);
+
+app.listen(7000, () => {
+  console.log("server started sucessfully");
+});

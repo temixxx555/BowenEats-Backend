4c7b86c18c07bfff6e8f77a2fcc871550ad3ec0e
Adebayo Temi <temiq3@gmail.com>
2024-09-17 18:46:46 +0100

    adding user profile

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import {v2 as cloudinary} from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 
 const app = express();
 app.use(express.json());
@@ -12,12 +14,17 @@ mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to database"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health ok!" });
 });
 
 app.use("/api/my/user", myUserRoute);
-
+app.use("/api/my/restaurant", myRestaurantRoute)
 app.listen(7000, () => {
   console.log("server started sucessfully");
 });

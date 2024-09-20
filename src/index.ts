500f1af6b027705d4497e9a30371de35a9b06e37
Adebayo Temi <temiq3@gmail.com>
2024-09-20 15:59:32 +0100

    added order stuffs

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
-import {v2 as cloudinary} from "cloudinary";
-import myRestaurantRoute from "./routes/MyRestaurantRoute"
-import restaurantRoute from "./routes/RestaurantRoute"
-
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+import OrderRoute from "./routes/OrderRoutes";
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+app.use(express.json());
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("connected to database"));
@@ -25,8 +27,9 @@ app.get("/health", async (req: Request, res: Response) => {
 });
 
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute)
-app.use("/api/restaurant", restaurantRoute)
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", OrderRoute);
 app.listen(7000, () => {
   console.log("server started sucessfully");
 });
